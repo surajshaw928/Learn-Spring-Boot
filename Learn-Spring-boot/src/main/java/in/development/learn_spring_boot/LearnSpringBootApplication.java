905c180a5113df20682e22a7ed9a210ccabb9d0e
SurajGW117 <suraj.shaw@greenwave.co.in>
2026-04-06 12:56:57 +0530

    initialize project

---
+++ Learn-Spring-boot/src/main/java/in/development/learn_spring_boot/LearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package in.development.learn_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
